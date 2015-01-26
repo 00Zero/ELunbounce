192f381e175a7cd09ad9968466321f17e72f4fd6
Adam Burgh <a_burgh@hotmail.com>
2015-01-26 10:54:21 -0200

    Create conversionTags.js

---
+++ conversionTags.js
@@ -0,0 +1,57 @@
+<!-- Tags to put on page that signifies a complete conversion (registration) -->
+
+<!-- Unbounce conversion, location: Before Body End Tag -->
+<script type="text/javascript"> 
+  var _ubaq = _ubaq || [];
+  _ubaq.push(['trackGoal', 'convert']);
+
+  (function() {
+  var ub_script = document.createElement('script');
+  ub_script.type = 'text/javascript';
+  ub_script.src = 
+    ('https:' == document.location.protocol ? 'https://' : 'http://') + 
+    'd3pkntwtp2ukl5.cloudfront.net/uba.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ub_script, s);
+  }) ();
+</script>
+
+<! -- Facebook conversion, location: Head -->
+<!-- Facebook Conversion Code for Unbounce registration -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6017581757077', {'value':'0.00','currency':'BRL'}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6017581757077&amp;cd[value]=0.00&amp;cd[currency]=BRL&amp;noscript=1" /></noscript>
+
+<! -- AdWords conversion, location: Body -->
+<!-- Google Code for Profile Sign Up (Candidate) Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1004776907;
+var google_conversion_language = "en";
+var google_conversion_format = "2";
+var google_conversion_color = "ffffcc";
+var google_conversion_label = "9vJFCLWH-AIQy9uO3wM";
+var google_conversion_value = 1.00;
+var google_conversion_currency = "BRL";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1004776907/?value=1.00&amp;currency_code=BRL&amp;label=9vJFCLWH-AIQy9uO3wM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
