5451a8f2b94fee3740daef664a93e90f707b9adc
Adam Burgh <a_burgh@hotmail.com>
2015-01-26 10:36:13 -0200

    Create trackingPixels.js

---
+++ trackingPixels.js
@@ -0,0 +1,40 @@
+<!-- All of the tracking pixels that should be on landing pages -->
+
+<!-- Google Analytics tag, Location: Head -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-31997141-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- Google Analytics tag to associate subdomain with parent domain, Location: Head -->
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-31997141-1']);
+_gaq.push(['_setDomainName', 'empregoligado.com.br']);
+_gaq.push(['_trackPageview']);
+</script>
+
+<!-- Facebook Website Custom Audience, Location: Before Body End Tag -->
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '1456018767983074']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1456018767983074&amp;ev=PixelInitialized" />
+</noscript>
