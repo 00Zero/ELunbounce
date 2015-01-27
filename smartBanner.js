33e96b7cae7051d392483d9f8a45b5aecc7040cc
Adam Burgh <a_burgh@hotmail.com>
2015-01-27 19:01:23 -0200

    Added dynamic passing of utms to Google Play

--- smartBanner.js
+++ smartBanner.js
@@ -8,15 +8,15 @@
     inAppStore: 'On the App Store', // Text of price for iOS
     inGooglePlay: 'Google Play', // Text of price for Android
     inWindowsStore: 'In the Windows Store', // Text of price for Windows
-    GooglePlayParams: 'utm_source=unbounce-popup', // Aditional parameters for the market
+    GooglePlayParams: encodeURIComponent((location.search.substr(1))+'&landing=unbounce+'+(window.location.pathname.substr(1, window.location.pathname.length-2))), // Aditional parameters for the market
     icon: 'https://lh3.ggpht.com/s6ZxMdRgeA_O8OPXJqrG_lWsviZiGTwjHE4gEAu4corVhC3WZdRdOrsQQqBziM1crF0', // The URL of the icon (defaults to <meta name="apple-touch-icon">)
     iconGloss: null, // Force gloss effect for iOS even for precomposed
     button: 'BAIXAR', // Text for the install button
     scale: 'auto', // Scale based on viewport size (set to 1 to disable)
     speedIn: 300, // Show animation speed of the banner
     speedOut: 400, // Close animation speed of the banner
-    daysHidden: 0, // Duration to hide the banner after being closed (0 = always show banner)
-    daysReminder: 0, // Duration to hide the banner after "VIEW" is clicked *separate from when the close button is clicked* (0 = always show banner)
+    daysHidden: 1, // Duration to hide the banner after being closed (0 = always show banner)
+    daysReminder: 1, // Duration to hide the banner after "VIEW" is clicked *separate from when the close button is clicked* (0 = always show banner)
     force: null, // Choose 'ios', 'android' or 'windows'. Don't do a browser check, just always show this banner
     hideOnInstall: false, // Hide the banner after "VIEW" is clicked.
     iOSUniversalApp: true, // If the iOS App is a universal app for both iPad and iPhone, display Smart Banner to iPad users, too.      
